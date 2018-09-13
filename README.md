9bc9cd24e0f299e4e1869af1ad2362e6ece2cbb5
a_liYa <liyaf@8531.cn>
2018-09-13 22:05:21 +0800

    添加ReadMe待优化部分

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lifecycle
 Android生命周期辅助管理类
+
+优化：
+
+添加Callback方法注解来实现
--- lifecycle/src/main/java/com/aliya/lifecycle/FragmentLifecycleCallbacks.java
+++ lifecycle/src/main/java/com/aliya/lifecycle/FragmentLifecycleCallbacks.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentManager;
  * @author a_liYa
  * @date 2018/5/16 16:59.
  */
-public abstract class FragmentLifecycleCallbacks extends FragmentManager.
-        FragmentLifecycleCallbacks {
+public abstract class FragmentLifecycleCallbacks extends FragmentManager.FragmentLifecycleCallbacks {
 
 }
